bed09d624ea1f696087e03bcdd442af9f520e5f2
brnkgabriel <brnkgabriel@gmail.com>
2018-12-04 11:10:30 +0100

    questions create-edit page 50% done

--- src/views/questions/Questions.vue
+++ src/views/questions/Questions.vue
@@ -1,17 +1,78 @@
 <template>
   <div class="questions">
-    <question/>
+    <question :question="selectedQuestion" />
+    <ul class="question-list">
+      <li
+      v-for="(question, index) in questions"
+      :key="index"
+      @click.prevent="selectQuestion(question)">
+        {{question.serial}}|{{question.stage}} {{question.question}}
+      </li>
+    </ul>
+    <div class="store">
+      <button @click.prevent="saveQuestions">Save Questions</button>
+    </div>
   </div>
 </template>
 
 <script>
-import Question from './question/Question.vue'
+import Question from "./question/Question.vue";
 export default {
   components: {
     Question
   },
-  data () {
-    return {}
+  data() {
+    return {
+      selectedQuestion: null,
+      questions: [
+        {
+          serial: 0,
+          stage: 0,
+          question: "#?",
+          options: [],
+          option: {
+            key: "#",
+            value: "#",
+            pts: 0
+          },
+          answer: null,
+          type: "#"
+        },
+        {
+          serial: 1,
+          img: '',
+          stage: 0,
+          question: "#?",
+          options: [],
+          option: {
+            key: "#",
+            value: "#",
+            pts: 0
+          },
+          answer: null,
+          type: "#"
+        }
+      ]
+    };
+  },
+  created() {
+    this.selectedQuestion = this.questions[0];
+  },
+  methods: {
+    selectQuestion(question) {
+      this.selectedQuestion = question;
+    },
+    saveQuestions() {
+      console.log('questions are', this.questions);
+    }
   }
-}
+};
 </script>
+
+<style scoped>
+.question-list,
+.question {
+  display: inline-block;
+  vertical-align: top;
+}
+</style>
--- src/views/questions/question/Question.vue
+++ src/views/questions/question/Question.vue
@@ -2,30 +2,52 @@
   <div class="question">
     <form>
       <div>
-        <label for="serial-number">Serial #</label>
-        <input type="number" id="serial-number"/>
+        <label for="serial">Serial #</label>
+        <input type="number" v-model="question.serial" id="serial"/>
       </div>
       <div>
         <label for="stage">Stage</label>
-        <input type="number" id="stage"/>
+        <input type="number" v-model="question.stage" id="stage"/>
+      </div>
+      <div v-if="question.img !== undefined">
+        <label for="image">Image</label>
+        <input type="file" name="image" id="image" accept="image/*">
+        <input type="submit">
       </div>
       <div>
         <label for="question">Question</label>
-        <textarea id="question" cols="30" rows="10" v-model="question"></textarea>
+        <textarea id="question" cols="30" rows="10" v-model="question.question"></textarea>
       </div>
       <div>
         <label for="option">Options</label>
-        <input type="text" id="option" v-model="option.value" />
-        <input type="number" name="pts" v-model="option.pts" />
+        <input type="text" id="option" v-model="question.option.value" />
+        <input type="number" name="pts" v-model="question.option.pts" />
         <button @click="storeOption">Store Option</button>
       </div>
       <ul>
-        <li v-for="(option, index) in options" :key="index">
+        <li v-for="(option, index) in question.options" :key="index">
           <button @click.prevent="selectAnswer(option)">{{ option.value }} ({{ option.pts }})</button> <button @click.prevent="deleteOption(option)">x</button>
         </li>
       </ul> 
       <div>
-        <label for="answer">Answer is: </label><span>{{answer}}</span>
+        <label for="answer">Answer is: </label><span>{{question.answer}}</span>
+      </div>
+      <div>
+        <label for="type">Type: </label>
+        <select v-model="question.type">
+          <option v-for="(type, index) in types" :key="index">{{type}}</option>
+        </select>
+      </div>
+      <div id="preview">
+        <h3>Preview Question</h3>
+        <p>Id is: {{question.id}}</p>
+        <p>Question is: {{question.question}}</p>
+        <p>Options:</p>
+        <ul>
+          <li v-for="(option, index) in question.options" :key="index">{{option}}</li>
+        </ul>
+        <p>Answer is: {{question.answer}}</p>
+        <p>Type is: {{question.type}}</p>
       </div>
     </form>
   </div>
@@ -33,36 +55,40 @@
 
 <script>
 export default {
+  props: ['question'],
   data() {
     return {
-      id: '#|#',
-      question: '#?',
-      options: [],
-      option: {
-        key: '#',
-        value: '#',
-        pts: 0
-      },
-      answer: null,
-      type: '#',
+      types: [
+        'worship',
+        'message',
+        'bible',
+        'books',
+        'picture'
+      ]
+    }
+  },
+  computed: {
+    id: function () {
+      console.log('hello')
+      return this.question.serial + '|' + this.question.stage
     }
   },
   methods: {
     storeOption(evt) {
       evt.preventDefault();
-      var optionValue = this.option.value.split(' ').join('-')
+      var optionValue = this.question.option.value.split(' ').join('-')
       console.log(optionValue);
-      this.options.push({
+      this.question.options.push({
         key: optionValue.toLowerCase(),
-        value: this.option.value,
-        pts: this.option.pts
+        value: this.question.option.value,
+        pts: this.question.option.pts
       })
     },
     selectAnswer(value) {
-      this.answer = value;
+      this.question.answer = value;
     },
     deleteOption(option) {
-      this.options = this.options.filter(opt => {
+      this.question.options = this.question.options.filter(opt => {
         return opt.key != option.key;
       })
     }
