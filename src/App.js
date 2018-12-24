abd7bee0fa2558ab594b6adef6fc852cc9588468
brnkgabriel <brnkgabriel@gmail.com>
2018-12-24 15:50:39 +0100

    completed sidebar nav

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Navbar from './components/Navbar.vue' 
+import NavMenu from "./nav-menu";
+export default {
+  components: {
+    Navbar
+  },
+  created() {
+    this.$store.dispatch('getStudents');
+    this.$store.dispatch('getQuestions');
+    this.$store.dispatch('getMaterials')
+  },
+  mounted() {
+    new NavMenu();
+  },
+}
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,92 +1,55 @@
 <template>
-  <div id="navbar">
+  <nav class="st-menu st-effect-5" id="menu-5">
+    <h2>Menu</h2>
     <ul>
-      <li>
-        <router-link to="/">Home</router-link>
-      </li>
-      <li v-if="isLoggedIn">
-        <router-link to="/profile">Profile</router-link>
-      </li>
-      <li v-if="isLoggedIn">
-        <router-link to="/quiz">Quiz</router-link>
-      </li>
-      <li v-if="isLoggedIn && student.roles_permissions.roles === 'admin'">
-        <router-link to="/questions">Questions</router-link>
-      </li>
-      <li v-if="isLoggedIn">
-        <router-link to="/rank">Rank</router-link>
-      </li> 
-      <li v-if="!isLoggedIn">
-        <router-link to="/login">Login</router-link>
-      </li>
-      <li v-if="!isLoggedIn">
-        <router-link to="/signup">Signup</router-link>
-      </li>
-      <li v-if="isLoggedIn">
-        <button @click="logout">Logout</button>
-      </li>
-      <li>
-        <router-link to="/about">About</router-link>
-      </li>
+      <li><router-link class="nav-link" to="/">Home</router-link></li>
+      <li v-if="isLoggedIn"><router-link class="nav-link" to="/profile">Profile</router-link></li>
+      <li v-if="isLoggedIn"><router-link class="nav-link" to="/quiz">Quiz</router-link></li>
+      <li v-if="isLoggedIn && student.roles_permissions.roles === 'admin'"><router-link class="nav-link" to="/questions">Questions</router-link></li>
+      <li v-if="isLoggedIn"><router-link class="nav-link" to="/rank">Rank</router-link></li> 
+      <li v-if="!isLoggedIn"><router-link class="nav-link" to="/login">Login</router-link></li>
+      <li v-if="!isLoggedIn"><router-link class="nav-link" to="/signup">Signup</router-link></li>
+      <li v-if="isLoggedIn"><button class="nav-link" @click="logout">Logout</button></li>
+      <li><router-link class="nav-link" to="/about">About</router-link></li>
     </ul>
-  </div>
+  </nav>
 </template>
 
 <script>
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import { bus } from '../main'
+import firebase from "firebase/app";
+import "firebase/auth";
+import { bus } from "../main";
 export default {
   name: "navbar",
   data() {
     return {
       isLoggedIn: false,
       student: null
-    }
+    };
   },
   created() {
-    this.student = JSON.parse(localStorage.getItem('student'))
-    this.isLoggedIn = !!localStorage.getItem('student');
-    bus.$on('isLoggedIn', (student) => {
+    this.student = JSON.parse(localStorage.getItem("student"));
+    this.isLoggedIn = !!localStorage.getItem("student");
+    bus.$on("isLoggedIn", student => {
       this.isLoggedIn = !!student;
       this.student = student;
-    })
+    });
   },
   methods: {
-    logout: function () {
-      firebase.auth().signOut()
-      .then(() => {
-        this.$store.commit('removeStudent');
-        this.isLoggedIn = false;
-        this.$router.push('/login')
-      })
+    logout: function() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$store.commit("removeStudent");
+          this.isLoggedIn = false;
+          this.$router.push("/login");
+        });
     }
   }
 };
 </script>
 
 <style scoped>
-ul {
-  padding: 0;
-  margin: 0;
-}
 
-li {
-  list-style: none;
-  display: inline-block;
-  margin: 0px 5px;
-}
-
-#navbar {
-  padding: 10px;
-}
-
-#navbar a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#navbar a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
